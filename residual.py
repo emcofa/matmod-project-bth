007b2aebf9ba0587aa736e8d48bffb11802a7cca
emcofah <emfh21@student.bth.se>
2023-01-01 21:57:31 +0100

    added comments

--- residual.py
+++ residual.py
@@ -11,30 +11,30 @@ import seaborn as sns
 def residual():
     """
     Uppgift 6 - Residualanalys
-    Beräkna residualerna, e= y−y^, för de två modellerna och plotta dessa. Hur ser de ut? Plotta 
-    residualerna mot normalfördelningen (i Python t.ex. genom Seaborn.distplot eller 
-    scipy.stats.probplot). Kommentera dessa plottar utseende och beskriv vilka slutsatser vi kan dra 
-    utifrån dessa. Finns det några beroenden? Hur väl följer residualerna en normalfördelning?  
+    Beräkna residualerna, e= y−y^, för de två modellerna och plotta dessa. Hur ser de ut? Plotta
+    residualerna mot normalfördelningen (i Python t.ex. genom Seaborn.distplot eller
+    scipy.stats.probplot). Kommentera dessa plottar utseende och beskriv vilka slutsatser vi kan dra
+    utifrån dessa. Finns det några beroenden? Hur väl följer residualerna en normalfördelning?
     Beräkna också deras varians och argumentera för vilken modell vi bör använda utifrån dina resultat.
     """
     # Läs in csv-filen och konvertera datevärdena till datumobjekt
-    df = pd.read_csv('./data/gladhammar.csv')
-    df['date'] = pd.to_datetime(df['date'])
-    df['date'] = mdates.date2num(df['date'])
-    
-    y = df['value']
-    x = df['date']
+    df = pd.read_csv("./data/gladhammar.csv")
+    df["date"] = pd.to_datetime(df["date"])
+    df["date"] = mdates.date2num(df["date"])
+
+    y = df["value"]
 
     y_min = np.min(y)
     constant = 10
-    
+
+    # Konvertera negativa värden till positiva
     if y_min < 0:
-        df['log_value'] = np.log(y - y_min + constant)
+        df["log_value"] = np.log(y - y_min + constant)
     else:
-        df['log_value'] = np.log(y)
-    
+        df["log_value"] = np.log(y)
+
     # Skapa och träna en ny regressionsmodell med 'log_value' som förklarande variabel och 'date' som beroende variabel
-    exp_model = smf.ols('log_value ~ date', df)
+    exp_model = smf.ols("log_value ~ date", df)
     exp_results = exp_model.fit()
 
     # Kondifensintervall 95%
@@ -43,46 +43,54 @@ def residual():
 
     # Transformera tillbaka modellen och förutsägelserna innan du plottar dem
     if y_min < 0:
-        exp_model = np.exp(exp_predictions['mean']) + y_min - constant
+        exp_model = np.exp(exp_predictions["mean"]) + y_min - constant
     else:
-        exp_model = np.exp(exp_predictions['mean'])
+        exp_model = np.exp(exp_predictions["mean"])
 
     # Skapa och träna en ny linjär regressionsmodell med 'date' som förklarande variabel och 'value' som beroende variabel
-    lin_model = smf.ols('value ~ date', df)
+    lin_model = smf.ols("value ~ date", df)
     lin_results = lin_model.fit()
 
     # Kondifensintervall 95%
     lin_predictions = lin_results.get_prediction(df).summary_frame(alpha)
-    
+
     # Beräkna residualerna för linjär modell 𝑒= 𝑦−𝑦̂
-    lin_residuals = df['value'] - lin_predictions['mean']
+    lin_residuals = df["value"] - lin_predictions["mean"]
 
     # Beräkna residualerna för exponentiell modell 𝑒= 𝑦−𝑦̂
-    exp_residuals = df['value'] - exp_model
+    exp_residuals = df["value"] - exp_model
 
-    date_formatter = mdates.DateFormatter('%Y-%m-%d')
+    date_formatter = mdates.DateFormatter("%Y-%m-%d")
     plt.gca().xaxis.set_major_formatter(date_formatter)
 
     # Plotta residualerna för linjär modell
-    plt.scatter(df['date'], lin_residuals, label='Linjär modell Residualer', s=6, color='blue')
-    plt.scatter(df['date'], exp_residuals, label='Exponentiell modell Residualer', s=6, color='green')
+    plt.scatter(
+        df["date"], lin_residuals, label="Linjär modell Residualer", s=6, color="blue"
+    )
+    plt.scatter(
+        df["date"],
+        exp_residuals,
+        label="Exponentiell modell Residualer",
+        s=6,
+        color="green",
+    )
 
-    plt.xlabel('Datum')
-    plt.ylabel('Residualer')
+    plt.xlabel("Datum")
+    plt.ylabel("Residualer")
     plt.legend()
     plt.show()
-    
+
     # Plotta residualerna för linjär modell mot normalfördelningen
-    sns.distplot(lin_residuals, label='Linjär modell residualer')
+    sns.distplot(lin_residuals, label="Linjär modell residualer")
 
     # Plotta residualerna för exponentiell modell mot normalfördelningen
-    sns.distplot(exp_residuals, label='Exponentiell modell residualer')
+    sns.distplot(exp_residuals, label="Exponentiell modell residualer")
 
-    plt.xlabel('Residualer')
-    plt.ylabel('Frekvens')
+    plt.xlabel("Residualer")
+    plt.ylabel("Frekvens")
     plt.legend()
     plt.show()
-    
+
     # Beräkna variansen för residualerna för linjär modell
     lin_variance = np.var(lin_residuals)
 
@@ -93,6 +101,10 @@ def residual():
     print("Linjär varians:", lin_variance)
     print("Transformerad varians:", exp_variance)
     if lin_variance < exp_variance:
-        print("Linjär modellen ger residualer med lägre varians än exponentiell modellen, så den kan vara lämpligare för data.")
+        print(
+            "Linjär modellen ger residualer med lägre varians än exponentiell modellen, så den kan vara lämpligare för data."
+        )
     else:
-        print("Exponentiell modellen ger residualer med lägre varians än linjär modellen, så den kan vara lämpligare för data.")
+        print(
+            "Exponentiell modellen ger residualer med lägre varians än linjär modellen, så den kan vara lämpligare för data."
+        )
